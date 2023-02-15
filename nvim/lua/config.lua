b5750077b60cffb46bcb7fd717d85b2c3cf3ee43
Johannes Walter <johannes@wltr.io>
2023-02-15 17:28:26 -0500

    Update

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -17,10 +17,12 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 require'lspconfig'.verible.setup{
   on_attach = on_attach,
   cmd = {"verible-verilog-ls", "--rules_config_search=true"}
 }
+
+--require'lspconfig'.clangd.setup{}
