92bfce6740c5a6403fd9e6c87ee2cfa1d2fd2904
Johannes Walter <johannes@wltr.io>
2024-05-16 10:10:54 -0400

    Update

--- nvim/lua/wltr/plugins/plugins.lua
+++ nvim/lua/wltr/plugins/plugins.lua
@@ -173,7 +173,7 @@ return {
         ensure_installed = {'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'bash', 'tcl', 'python'},
         highlight = {
           enable = true,
-          disable = not require('wltr.largefile').enable_except_large_or_diff,
+          disable = not require('wltr.largefile').enable_except_large_or_diff(),
           additional_vim_regex_highlighting = false
         }
       }
