f3137e977800d94714b3d320d1a38f2ffd9a1704
Johannes Walter <johannes@wltr.io>
2024-05-16 14:11:47 -0400

    Update

--- nvim/lua/wltr/plugins/plugins.lua
+++ nvim/lua/wltr/plugins/plugins.lua
@@ -170,7 +170,9 @@ return {
     config = function()
       local configs = require('nvim-treesitter.configs')
       configs.setup{
-        ensure_installed = {'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'bash', 'tcl', 'python', 'gitcommit', 'verilog'},
+        ensure_installed = {
+          'c', 'cpp', 'lua', 'vim', 'vimdoc', 'query', 'bash', 'tcl', 'python', 'gitcommit', 'verilog', 'yaml', 'json'
+        },
         highlight = {
           enable = true,
           disable = not require('wltr.largefile').enable_except_large_or_diff(),
