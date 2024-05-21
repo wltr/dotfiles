dc23497dfeafa8c9b7bbff7e604dc3509798264a
Johannes Walter <johannes@wltr.io>
2024-05-21 17:41:07 -0400

    Update

--- nvim/lua/wltr/keymap.lua
+++ nvim/lua/wltr/keymap.lua
@@ -11,20 +11,24 @@ keymap('n', '[d', vim.diagnostic.goto_prev)
 keymap('n', ']d', vim.diagnostic.goto_next)
 keymap('n', '<space>q', vim.diagnostic.setloclist)
 
-keymap('n', '<C-N>', ':NvimTreeToggle<CR>')
-keymap('n', '<C-M-N>', ':NvimTreeFindFile<CR>')
+keymap('n', '<leader>n', ':NvimTreeToggle<CR>')
+keymap('n', '<leader>m', ':NvimTreeFindFile<CR>')
 
-keymap('n', '<tab>', ':bnext<CR>')
-keymap('n', '<S-tab>', ':bprevious<CR>')
+keymap('n', '<S-tab>', ':b#<CR>')
+keymap('n', '<C-N>', ':bnext<CR>')
+keymap('n', '<C-P>', ':bprevious<CR>')
 keymap('n', '<C-Q>', ':BDelete this<CR>')
+
 keymap('n', '<C-J>', function() vim.cmd('wincmd j') end)
 keymap('n', '<C-K>', function() vim.cmd('wincmd k') end)
 keymap('n', '<C-L>', function() vim.cmd('wincmd l') end)
 keymap('n', '<C-H>', function() vim.cmd('wincmd h') end)
+
 keymap('n', '<C-W><C-J>', ':exe "resize +5"<CR>')
 keymap('n', '<C-W><C-K>', ':exe "resize -5"<CR>')
 keymap('n', '<C-W><C-L>', ':exe "vertical resize +5"<CR>')
 keymap('n', '<C-W><C-H>', ':exe "vertical resize -5"<CR>')
+
 keymap('n', '<C-T><tab>', ':tabnext<CR>')
 keymap('n', '<C-T><C-N>', ':tabnew<CR>')
 keymap('n', '<C-T><C-Q>', ':tabclose<CR>')
@@ -35,7 +39,7 @@ keymap('n', 'gs', ':ClangdSwitchSourceHeader<CR>')
 
 keymap('t', '<leader>e', '<C-\\><C-n>')
 
-keymap('n', '<C-P>', require('telescope.builtin').git_files)
+keymap('n', '<leader>p', require('telescope.builtin').git_files)
 keymap('n', '<leader>ff', require('telescope.builtin').find_files)
 keymap('n', '<leader>fd', require('telescope.builtin').lsp_document_symbols)
 keymap('n', '<leader>fg', require('telescope.builtin').live_grep)
